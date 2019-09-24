69a717df321010769ed4c70f7995be93ec23a0da
April Anderson <april.anderson.2022@student.lhschools.org>
2019-09-24 10:02:05 -0400

    deleted unused .java file, declared variables and initialized

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -4,5 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        String firstWord;
+        String secondWord;
+
+        firstWord = "dog";
+        secondWord = "house";
+
+        System.out.println(firstWord+secondWord);
     }
 }
